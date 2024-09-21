e3eca19b4e9e316d43838634bd4aee2cb60e0a72
ashabb <ashishgole99955@gmail.com>
2024-09-21 21:14:56 +0530

    mongodb databse connected

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## Ongoing Project
+
+NEXTjs help - https://nextjs.org/docs/app/api-reference/functions/next-response
--- app/api/product/route.js
+++ app/api/product/route.js
@@ -1,5 +1,26 @@
 import { NextResponse } from 'next/server'
 
-export async function GET(request) {
-  return NextResponse.json({ name: 'ash gole' }, { status: 500 })
+// export async function GET(request) {
+//   return NextResponse.json({ name: 'ash gole' }, { status: 500 })
+// }
+
+import { MongoClient } from 'mongodb';
+
+const uri = process.env.NEXT_PUBLIC_MONGODB_URI; // MongoDB connection string
+const client = new MongoClient(uri);
+
+export async function GET(req) {
+  try {
+    await client.connect();
+    const database = client.db('demodb');
+    const usersCollection = database.collection('users');
+
+    const users = await usersCollection.find({}).toArray();
+    return NextResponse.json(users);
+  } catch (error) {
+    return NextResponse.json({ message: 'Internal Server Error', error });
+
+  } finally {
+    await client.close();
+  }
 }
