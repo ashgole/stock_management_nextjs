2e027a351550b4f132b86e5c6b97fac882def440
ashabb <ashishgole99955@gmail.com>
2024-09-22 10:10:51 +0530

    u

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import Header from "./components/Header";
-import Dashboard from "./pages/dashboard/Dashboard";
+// import Dashboard from "./pages/dashboard/Dashboard";
 
 export default function Home() {
   return (
     <>
       <Header />
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <Dashboard />
+        {/* <Dashboard /> */}
       </div>
     </>
   );
