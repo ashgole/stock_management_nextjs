f7f4ce83c01e9c7f7430d8ef6c2e1ff6f54d899b
ashabb <ashishgole99955@gmail.com>
2024-09-22 10:14:39 +0530

    u

--- app/page.js
+++ app/page.js
@@ -1,13 +1,9 @@
-import Header from "./components/Header";
-// import Dashboard from "./pages/dashboard/Dashboard";
+
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        {/* <Dashboard /> */}
-      </div>
+       ashabb
     </>
   );
 }
